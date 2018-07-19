bad83d1eb42ba97453e16f20fde41fa1c576dfc7
Clayton Blythe <clayton.blythe@asurion.com>
2018-07-19 10:01:51 -0700

    updated code

--- code/store_data.py
+++ code/store_data.py
@@ -6,9 +6,11 @@ import random
 
 
 def check_database_exists():
-    """Create the database transactions.db if it does not exist.
+    """
+    Create the database transactions.db if it does not exist.
     Here the UNIQUE constraint will maintain we don't insert duplicate
-    payment_id's into the table in that database"""
+    payment_id's into the table in that database
+    """
     if not os.path.exists('../data/transactions.db'):
         conn = sqlite3.connect('../data/transactions.db')
         c = conn.cursor()
@@ -22,7 +24,7 @@ def check_database_exists():
 
 
 def get_vens(num=1000):
-    """Make a single request to the API endpoint for lots of payments...Kind of scary this data is public"""
+    """Make a single request to the Public API endpoint for lots of payments...Kind of scary this data is public"""
     url = f"https://venmo.com/api/v5/public?limit={num}"
     try:
         vens = requests.get(url).json()['data']
@@ -43,6 +45,7 @@ def insert_vens(vens, cursor):
             mentions, permalink, story_id = d['mentions'], d['permalink'], d['story_id']
             transactions, transaction_type, updated_time = d['transactions'], d['type'], \
                                                            d['updated_time']
+            print_ven_to_console(payment_id, actor, transactions, message, cursor)
             cursor.execute(
                 'INSERT OR REPLACE INTO transactions VALUES (?,?,?,?,?,?,?,?,?,?)', (payment_id, str(actor),
                                                                                      created_time, message,
@@ -55,35 +58,27 @@ def insert_vens(vens, cursor):
             print(f"Exception inserting to database {e}")
 
 
-def print_vens_to_console(vens, cursor):
-
-    for ven in vens:
-        try:
-            d = dict(ven)
-            payment_id, actor, created_time = d['payment_id'], d['actor'], d['created_time']
-            message, comments = d['message'], d['comments']
-            transactions, transaction_type, updated_time = d['transactions'], d['type'], \
-                                                           d['updated_time']
-
-            cursor.execute("SELECT * FROM transactions WHERE payment_id = ?", (payment_id,))
-            data = cursor.fetchone()
-            if not data:
-                print(f"{actor['username']} ----> {transactions[0]['target']['username']}\n{message}\n")
-            time.sleep(.01)
-        except (TypeError, KeyError) as e:
-            print(f"Exception: {e}")
+def print_ven_to_console(payment_id, actor, transactions, message, cursor):
+    """Print transaction w/ Emoji to console.. Unix & Mac should work for sure"""
+    try:
+        cursor.execute("SELECT * FROM transactions WHERE payment_id = ?", (payment_id,))
+        data = cursor.fetchone()
+        if not data:
+            print(f"{actor['username']} ----> {transactions[0]['target']['username']}\n{message}\n")
+        time.sleep(.01)
+    except (TypeError, KeyError) as e:
+        print(f"Exception: {e}")
 
 
 def stream_and_insert(num, cursor):
-    """Continous stream of venmo payments to your a sqlite database & your terminal"""
-    print("\nBeginning venmo payment stream\n")
-    print('-----------------------------------------------------------')
+    """Continuous stream of Venmo payments to your a sqlite database & your terminal"""
     while True:
         try:
             vens = get_vens(num)
             if vens:
-                print_vens_to_console(vens, cursor=cursor)
                 insert_vens(vens, cursor=cursor)
+            # Sleeping for random integer value between 10 and 20, doing some type of
+            # background fetching & consuming from a queue might work better
             time.sleep(random.randint(10, 20))
 
         except KeyboardInterrupt:
